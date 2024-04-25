06dd5d97f3c79d7b92df834de929e490f28a4a25
AUCHERENathan <important-apps.wrgny@simplelogin.co>
2024-04-25 09:14:00 +0200

    fix Location imports

--- ADEenMieux/src/pack/Location.java
+++ ADEenMieux/src/pack/Location.java
@@ -1,3 +1,9 @@
+package pack;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Location {
@@ -6,7 +12,7 @@ public class Location {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 
-    private string name;
+    private String name;
     private int capacity;
     private Agenda agenda;
 }
