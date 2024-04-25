1970a78051b8e77102323ea75630edd1f8131654
AUCHERENathan <important-apps.wrgny@simplelogin.co>
2024-04-25 09:12:18 +0200

    fix Task imports

--- ADEenMieux/src/pack/Task.java
+++ ADEenMieux/src/pack/Task.java
@@ -1,4 +1,11 @@
+package pack;
+
 import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Task {
@@ -7,7 +14,7 @@ public class Task {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;    
 
-    private string name;
+    private String name;
     
     private int deadline;
 
