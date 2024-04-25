f5c9d23060bfee99439cda406f3d5bc8f376a281
AUCHERENathan <important-apps.wrgny@simplelogin.co>
2024-04-25 09:10:16 +0200

    fix imports User

--- ADEenMieux/src/pack/User.java
+++ ADEenMieux/src/pack/User.java
@@ -1,6 +1,11 @@
+package pack;
+
 import java.util.Collection;
 
-import javax.annotation.processing.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class User {
