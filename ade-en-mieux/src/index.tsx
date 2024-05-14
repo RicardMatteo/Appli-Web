bb6ddb8159bf2ed41ff595697d56efc27d608890
AUCHERENathan <important-apps.wrgny@simplelogin.co>
2024-05-14 13:55:36 +0200

    ajout des dépendances et router

--- ade-en-mieux/src/index.tsx
+++ ade-en-mieux/src/index.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-//import Main from './Main';
 import reportWebVitals from "./reportWebVitals";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <React.StrictMode>
-      <App />
+      <RouterProvider router={router} />
     </React.StrictMode>
   );
 }
