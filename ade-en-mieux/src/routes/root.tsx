9f7df6f09b68fe2970c086f22a1aecb6ac61d17c
Mattéo Ricard <ricardm@bde.enseeiht.fr>
2024-05-16 10:20:57 +0000

    feat: background + colors

--- ade-en-mieux/src/routes/root.tsx
+++ ade-en-mieux/src/routes/root.tsx
@@ -1,11 +1,22 @@
 import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
 
+const HeroPattern = ({
+  pttrn,
+  children,
+}: {
+  pttrn: string;
+  children: React.ReactNode;
+}) => <div className={pttrn}>{children}</div>;
+
 export default function Root() {
   const navigate = useNavigate();
 
   return (
     <>
+      <HeroPattern pttrn={"topography-pattern"}>
+        <div></div>
+      </HeroPattern>
       <div className="container">
         <div>
           <h1>ADE en Mieux</h1>
@@ -13,8 +24,10 @@ export default function Root() {
         <div>
           <h2>Ceci sera la future homepage du site !</h2>
         </div>
-        <div className="button-container">
-          <button onClick={() => navigate("/tp3")}>TP3</button>
+        <div>
+          <button className="button-64" onClick={() => navigate("/tp3")}>
+            <span className="text">TP3</span>
+          </button>
           <button onClick={() => navigate("/login")}>Login</button>
           <button onClick={() => navigate("/register")}>Register</button>
           <button onClick={() => Cookies.remove("authToken")}>Logout</button>
