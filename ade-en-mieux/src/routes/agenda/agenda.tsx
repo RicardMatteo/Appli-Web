b041e72b5371f465e05409609fe7f27553e7fef3
AUCHERENathan <important-apps.wrgny@simplelogin.co>
2024-05-16 08:52:05 +0200

    base du dashboard

--- ade-en-mieux/src/routes/agenda/agenda.tsx
+++ ade-en-mieux/src/routes/agenda/agenda.tsx
@@ -1,8 +1,10 @@
 function Agenda() {
   return (
-    <div>
-      <h1>Agenda</h1>
-    </div>
+    <>
+      <div>
+        <h1>Agenda</h1>
+      </div>
+    </>
   );
 }
 
--- ade-en-mieux/src/routes/dashboard/dashboard.tsx
+++ ade-en-mieux/src/routes/dashboard/dashboard.tsx
@@ -1,9 +1,21 @@
+import { useNavigate } from "react-router-dom";
+
 function Dashboard() {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Welcome to your dashboard</p>
-    </div>
+    <>
+      <div>
+        <h1>Dashboard</h1>
+        <p>Welcome to your dashboard</p>
+      </div>
+
+      <div className="button-container">
+        <button onClick={() => navigate("/agenda")}>Agenda</button>
+        <button onClick={() => navigate("/organise")}>Orginise</button>
+        <button onClick={() => navigate("/participate")}>Participate</button>
+      </div>
+    </>
   );
 }
 
