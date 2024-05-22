179278636e916cb7b5bfc49d79016fb9c4bf9302
Nin7o <nin7o.g@proton.me>
2024-05-22 17:24:19 +0200

    le get with param est sensé marcher mais le sort me déteste

--- ADEenMieux/src/pack/Facade.java
+++ ADEenMieux/src/pack/Facade.java
@@ -11,6 +11,7 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -427,6 +428,22 @@ public class Facade {
 	@Consumes({ "application/json" })
 	public void ajoutAdresse(Adresse adresse) {
 		em.persist(adresse);
+	}
+
+	/**
+	 * @param cookie
+	 * @return User
+	 */
+	@GET
+	@Path("/getwcookie")
+	@Produces({ "application/json" })
+	public User getUserWithCookie(@HeaderParam("cookie") String cookie) {
+		System.out.println("Cookie : " + cookie);
+		TypedQuery<ConnexionToken> req = em.createQuery("select c from ConnexionToken c where c.token = :token",
+				ConnexionToken.class);
+		req.setParameter("token", cookie);
+		ConnexionToken token = req.getSingleResult();
+		return token.getUserToken();
 	}
 
 	/**
