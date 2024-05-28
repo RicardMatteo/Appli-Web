c3dddc246369b039fd18b010ce6d62c329d2c310
Mattéo Ricard <ricardm@bde.enseeiht.fr>
2024-05-28 13:48:48 +0000

    small fix

--- ade-en-mieux/src/routes/events/events.tsx
+++ ade-en-mieux/src/routes/events/events.tsx
@@ -354,13 +354,13 @@ function Events() {
           places={places}
           setPlaces={setPlaces}
         />
-        <ListSlots listSlot={listSlot} />
         <CreateEvent
           listSlot={listSlot}
           listUser={listUser}
           setListSlot={setListSlot}
         />
       </div>
+      <ListSlots listSlot={listSlot} />
     </>
   );
 }
